131796d0c065f9e1c7bfec7eefb69971da3fff4f
Sumi Seo <sumseo@paul-f4Br4s4.clusters.42paris.fr>
2025-02-17 15:09:55 +0100

    fix/span

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "Span.hpp"
+#include <ctime>
+#include <cstdlib>
 
 int main()
 {
@@ -11,8 +13,20 @@ int main()
         sp.addNumber(17);
         sp.addNumber(9);
         sp.addNumber(11);
+        // sp.addNumber(101);
         std::cout << sp.shortestSpan() << std::endl;
         std::cout << sp.longestSpan() << std::endl;
+
+        Span largeSpan(10000);
+        std::srand(std::clock()); 
+
+        for (int i = 0; i < 10000; i++) 
+        {
+            largeSpan.addNumber(std::rand() % 10000); 
+        }
+
+        std::cout << "Shortest Span : " << largeSpan.shortestSpan() << std::endl;
+        std::cout << "Longest Span : " << largeSpan.longestSpan() << std::endl;
     }
     catch(std::exception &e)
     {
